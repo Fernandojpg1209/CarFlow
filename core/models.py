e5e6422c749690af3e66b89051787bd706114877
leticia.rosa <leticiaferreirarosa02@gmail.com>
2026-04-04 13:25:57 -0300

    Projeto funcionando e atualizado, nao funciona o cadastro

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    duracao = models.IntegerField()  # minutos
+
+    def __str__(self):
+        return self.nome
+
+
+class Agendamento(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    data = models.DateField()
+    hora = models.TimeField()
+
+    def __str__(self):
+        return f"{self.usuario} - {self.servico} - {self.data}"
--- core/views.py
+++ core/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
-
 def cadastro(request):
     return render(request, 'cadastro.html')
